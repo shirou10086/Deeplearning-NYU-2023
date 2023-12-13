493d6cf6754d06390889ac612521f7ea5cb56c59
Jinli Duan <jinlid@uci.edu>
2023-12-13 08:58:51 -0500

    qq

--- nlp/preprocess.py
+++ nlp/preprocess.py
@@ -10,11 +10,6 @@ csv_files = [os.path.join(folder_path, file) for file in os.listdir(folder_path)
 # 合并所有 CSV 文件
 all_data = pd.concat([pd.read_csv(file) for file in csv_files])
 
-# 删除 'image' 和 'file_name' 列（如果存在）
-if 'image' in all_data.columns:
-    all_data.drop(columns=['image'], inplace=True)
-if 'file_name' in all_data.columns:
-    all_data.drop(columns=['file_name'], inplace=True)
 
 # 保存到新的 CSV 文件
 merged_file_path = './merged_dataset.csv'
